1f096e84a9a2ddb36c7fc3df38b4e762eb93a35f
MBSkipper <trawler22@gmail.com>
2026-03-06 09:58:24 +0000

    isPostAuthor middleware tested on patch & delete routes and is working

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const upload = require('../middlewares/upload') // enables use of file upload if needed 
-const { isAuthenticated } = require('../middlewares/auth.middlewares')
+const { 
+    isAuthenticated, 
+    isPostAuthor
+} = require('../middlewares/auth.middlewares')
 
 const {
     fetchPosts,
@@ -17,9 +20,9 @@ router.get('/posts',fetchPosts, )
 
 router.post('/posts', createPost)
 
-router.patch('/posts/:id', updatePost)
+router.patch('/posts/:id', isPostAuthor, updatePost)
 
-router.delete('/posts/:id', deletePost)
+router.delete('/posts/:id', isPostAuthor, deletePost)
 
 router.post('/posts/:id/like-toggle', togglePostLike)
 
